4f824c8b84fa65c7f023716e68394cd55d1c347d
Lasse <77295879+lassejlv@users.noreply.github.com>
2026-09-14 22:45:58 +0200

    Delete cloudflare/origin-headers directory

--- cloudflare/origin-headers/src/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const headers = new Headers(request.headers);
-    if (env.ORIGIN_SECRET) {
-      headers.set("x-analytics-origin-key", env.ORIGIN_SECRET);
-    }
-    return fetch(new Request(request, { headers }));
-  },
-};
